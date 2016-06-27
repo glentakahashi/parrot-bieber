e1fece58dc7161b143471e6881b58ea42be5cf16
Glen Takahashi <me@glentaka.com>
2016-06-27 14:27:07 -0400

    Initial commit

---
+++ bieber.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function abs_path {
+  (cd "$(dirname '$1')" &>/dev/null && printf "%s/%s" "$PWD" "${1##*/}")
+}
+
+mkdir -p ~/.tmp
+cd ~/.tmp
+wget --no-check-certificate https://github.com/jmhobbs/terminal-parrot/releases/download/1.0.1/terminal-parrot-darwin-amd64.zip
+unzip -f terminal-parrot-darwin-amd64.zip
+
+
+case $SHELL in
+  */zsh)
+    echo 'export PATH=$PATH:'$(abs_path ~/.tmp/terminal-parrot) >> ~/.zshrc
+    echo 'alias ls='"'"'([[ $RANDOM -lt 2048 ]] && terminal-parrot -loops 3); ls'"'"'' >> ~/.zshrc
+    clear
+    ;;
+  */bash)
+    echo 'export PATH=$PATH:'$(abs_path ~/.tmp/terminal-parrot) >> ~/.bashrc
+    echo 'alias ls='"'"'([[ $RANDOM -lt 2048 ]] && terminal-parrot -loops 3); ls'"'"'' >> ~/.bashrc
+    clear
+    ;;
+  *)
+    # assume something else
+esac
---
+++ readme.md
@@ -0,0 +1,6 @@
+# One line script for biebering your friends
+
+Has a 1/16 probability of parroting them when they ls.
+
+If you trust me (and you shouldn't) use this one line to automatically set up the prank:
+`curl -s https://raw.githubusercontent.com/glentakahashi/parrot-bieber/master/bieber.sh | sh`
